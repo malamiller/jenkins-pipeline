e71ac5d1e95b07573383e3d8b6c2e517faf8f9b4
emmabongo <malawimiller2015@gmail@gmail.com>
2024-07-16 15:52:54 -0400

    updates

--- 2-terraform-eks-deployment/backend.tf
+++ 2-terraform-eks-deployment/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-app-kub-2024-v2"
-    region = "eu-west-3"
+    bucket = "jenkins-app-kub-july-2024-v2.1"
+    region = "us-east-1"
     key = "eks/terraform.tfstate"
   }
 }
